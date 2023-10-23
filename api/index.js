9b7d3b39be97f5bd8df6e760ed292d461cfff9a7
pallavidhakne <pmdhakne2002@gmail.com>
2023-10-24 02:07:54 +0530

    connect database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
 import express from "express";
 //import dotenv from 'dotenv';
+import mongoose from 'mongoose'
 //dotenv.config();
-
+//connect database
+mongoose
+  .connect("mongodb+srv://pallavi:pallavi@crudauth.cqigqcr.mongodb.net/")
+ 
+ .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 //const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
