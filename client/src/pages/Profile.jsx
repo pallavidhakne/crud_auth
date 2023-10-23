bec427d75e87f83c1aaaebcc6116d39fb9517a0f
pallavidhakne <pmdhakne2002@gmail.com>
2023-10-23 12:29:10 +0530

    added routes and create pages

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      <h1>this is a Profile</h1>
+    </div>
+  )
+}
+
+export default Profile
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <div>
+      <h1>this is a SignIn</h1>
+    </div>
+  )
+}
+
+export default SignIn
